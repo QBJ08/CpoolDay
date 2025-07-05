0cef85ee5aa105b80cbc001d0b352291074fd23b
QBJ08 <quent1bj@gmail.com>
2025-07-05 14:48:20 +0200

    ouygbiugh

--- my_average.c
+++ my_average.c
@@ -1,6 +1,6 @@
 float my_average(int *grades, int count) {
-    int total = 0;
-    float average;
+    long long int total = 0;
+    double average;
 
     for (int i = 0; i<count; i++) {
         total += grades[i];
