15891ae9359379a92e00f3b076dd899b96101714
QBJ08 <quent1bj@gmail.com>
2025-07-05 14:27:13 +0200

    aosudgbiu

--- my_little_bistro.c
+++ my_little_bistro.c
@@ -1,23 +1,37 @@
-int my_little_bistro(int value1, char op, int value2) {
+int my_little_bistro(int value1, char op, int value2)
+{
+
+    if (!value1 | !value2)
+    {
+        return 0;
+    }
+
     switch (op)
     {
     case '+':
-        return value1+value2;
-    
+        return value1 + value2;
+
     case '-':
-        return value1-value2;
+        return value1 - value2;
 
     case '*':
-        return value1*value2;
-    
+        return value1 * value2;
+
     case '/':
-        return value1/value2;
-    
+        if (value2 == 0)
+        {
+            return 0;
+        }
+        return value1 / value2;
+
     case '%':
-        return value1%value2;
+        if (value2 == 0)
+        {
+            return 0;
+        }
+        return value1 % value2;
 
     default:
-        break;
+        return 0;
     }
 };
-
