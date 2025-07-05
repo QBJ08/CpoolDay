952dcf0b73e8ce28035d0721ac6558cb8ec9183a
QBJ08 <quent1bj@gmail.com>
2025-07-05 14:56:51 +0200

    oausgdiyvbh

---
+++ my_get_words.c
@@ -0,0 +1,17 @@
+#include "print_char.h"
+
+void my_get_words(char *sentance) {
+    int length = 0;
+    while (sentance[length]) length++;
+
+    for (int i = 0; i < length; i++) {
+        print_char(sentance[i]);
+        
+        if (sentance[i] == '\t' | sentance[i] == ' ') {
+            print_char('\n');
+        }
+        while (sentance[i] == '\t' | sentance[i] == ' ') {
+            i++;
+        }
+    }
+};
