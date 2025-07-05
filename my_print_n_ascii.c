300bbe67bc7a307e1c9f02b5c54de792060e4402
Bam_ <105546127+QBJ08@users.noreply.github.com>
2025-07-05 11:41:25 +0200

    my_print_n_ascii.c v2

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -4,8 +4,17 @@ void my_print_n_ascii(int howMany){
     if (howMany<0) {
         howMany = 0;
     }
+    else if (howMany>96) {
+        howMany = 0;
+    }
     for (int i=32; i<33+howMany; i++){
         print_char(i);
     }
 
 };
+
+int main(void)
+{
+my_print_n_ascii(5);
+return 0;
+}
