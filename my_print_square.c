cf691a88ab4a2fc774272973d7dbdd98b8bfcb79
QBJ08 <quent1bj@gmail.com>
2025-07-05 14:45:31 +0200

    ougiub

--- my_print_square.c
+++ my_print_square.c
@@ -1,15 +1,29 @@
 #include "print_char.h"
 
 void my_print_square(int size, char c) {
+    if (size < 1) {
+        return;
+    }
 
-    if (size < 0) {
+    if (c <= 32 | c >= 127) {
         return;
     }
 
     for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
+        if (i == 0 | i == size-1)
+            {
+            for (int j = 0; j < size; j++) {
+                print_char(c);
+            }
+        }
+
+        else {
+            print_char(c);
+            for (int j = 1; j < size-1; j++) {
+                print_char(' ');
+            }
             print_char(c);
         }
-        print_char("\n");
+        print_char('\n');
     }
 };
