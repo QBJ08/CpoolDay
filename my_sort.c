10d0e89b05b4e24fec2025345047e75f11c90c62
QBJ08 <quent1bj@gmail.com>
2025-07-05 15:26:36 +0200

    oaiz

--- my_sort.c
+++ my_sort.c
@@ -1,6 +1,9 @@
+#include "print_number.h"
+
 void my_sort(int *nlist, int size) {
     int tmp;
-    for (int i = 0;  i < size; i++) {
+    int i;
+    for (i = 0;  i < size; i++) {
         int mini = i;
         for (int j = i+1; j <= size; j++) {
             if (nlist[j] < nlist[mini]) {
@@ -10,5 +13,8 @@ void my_sort(int *nlist, int size) {
         tmp = nlist[i];
         nlist[i] = nlist[mini];
         nlist[mini] = tmp;
+    }
+    for (i = 0; i < size; i++) {
+        print_number(nlist[i]);
     }
 }
