4da61cb90b63580f4eb0c8e184330126b19934d8
QBJ08 <quent1bj@gmail.com>
2025-07-05 14:32:14 +0200

    auisvf

---
+++ my_average.c
@@ -0,0 +1,12 @@
+float my_average(int *grades, int count) {
+    int total = 0;
+    float average;
+
+    for (int i = 0; i<count; i++) {
+        total += grades[i];
+    }
+  
+    average = total/count;
+    return average;
+    
+};
--- my_print_square.c
+++ my_print_square.c
@@ -2,6 +2,10 @@
 
 void my_print_square(int size, char c) {
 
+    if (size < 0) {
+        return;
+    }
+
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             print_char(c);
